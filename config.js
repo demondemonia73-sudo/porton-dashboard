c210da38366d0c169bf1c47249d561c3776df2cf
demondemonia73-sudo <demondemonia73@gmail.com>
2026-04-05 14:43:12 -0400

    Update config.js

--- config.js
+++ config.js
@@ -12,11 +12,5 @@ const CONFIG = {
     },
     tiempos: {
         heartbeatTimeout: 30000
-    },
-    horario: {
-        inicioHora: 7,
-        inicioMinuto: 30,
-        finHora: 14,
-        finMinuto: 0
     }
 };
